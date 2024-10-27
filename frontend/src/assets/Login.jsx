81c1172664312d4138657a93d1e9299149234b26
jitendra kumar <128616359+jittupal@users.noreply.github.com>
2024-10-27 16:16:01 +0530

    Login.jsx

--- frontend/src/assets/Login.jsx
+++ frontend/src/assets/Login.jsx
@@ -33,7 +33,7 @@ const Login = () => {
   const Hdleansubmit= async (e) =>{
    e.preventDefault();
    try {
-    const response = await fetch(`http://localhost:5000/api/auth/login`, {
+    const response = await fetch(`https://codemon-i5f0.onrender.com/api/auth/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
