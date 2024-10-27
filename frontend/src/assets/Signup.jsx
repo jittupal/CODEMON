2b8a46323922211a4c92be10ea380664165e14b3
jitendra kumar <128616359+jittupal@users.noreply.github.com>
2024-10-27 16:16:31 +0530

    Signup.jsx

--- frontend/src/assets/Signup.jsx
+++ frontend/src/assets/Signup.jsx
@@ -35,7 +35,7 @@ const Signup = () => {
     // console.log(user);
     try {
       
-      const response = await fetch(`http://localhost:5000/api/auth/register`, {
+      const response = await fetch(`https://codemon-i5f0.onrender.com/api/auth/register`, {
         method:"POST",
         headers:{
           "Content-Type":"application/json"
